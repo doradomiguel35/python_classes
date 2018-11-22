68c1986c75e83ff42e2570888c7b69f5780a323f
Miguel Dorado <doradomiguel35@gmail.com>
2018-11-22 18:56:21 +0800

    some classes not yet called

---
+++ dictionaries_set_call_class.py
@@ -0,0 +1,8 @@
+from dictionaries_set_class import math_opt_dict
+
+operation = math_opt_dict({'num1': 1,'num2':3})
+print(operation.dict_add_values())
+print(operation.dict_sub_values())
+print(operation.dict_multiply_values())
+print(operation.dict_divide_values())
+
